8617825c947b2b906eabe6242f9a5632ded2ad33
Jin Qing <jinq0123@163.com>
2017-04-23 19:27:43 +0800

    Change GetMessageField().

--- src/detail/GetMessageField.cpp
+++ src/detail/GetMessageField.cpp
@@ -55,84 +55,81 @@ void SetTblMessageElement(LuaRef& rTbl, int index, const Message& msg)
 // Map field is supported.
 // Returns empty if success.
 string GetRepeatedFieldElement(const Message& msg,
-    const FieldDescriptor* pField, int index, LuaRef& rTbl)
+    const FieldDescriptor& field, int index, LuaRef& rTbl)
 {
-    assert(pField);
-    assert(pField->is_repeated());
+    assert(field.is_repeated());
     assert(index >= 0);
     const Reflection* pRefl = msg.GetReflection();
     assert(pRefl);
-    assert(index < pRefl->FieldSize(msg, pField));
+    assert(index < pRefl->FieldSize(msg, &field));
 
     using Fd = FieldDescriptor;
-    Fd::CppType eCppType = pField->cpp_type();
+    Fd::CppType eCppType = field.cpp_type();
     switch (eCppType)
     {
     case Fd::CPPTYPE_INT32:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedInt32(msg, pField, index));
+            pRefl->GetRepeatedInt32(msg, &field, index));
         return "";
     case Fd::CPPTYPE_INT64:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedInt64(msg, pField, index));
+            pRefl->GetRepeatedInt64(msg, &field, index));
         return "";
     case Fd::CPPTYPE_UINT32:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedUInt32(msg, pField, index));
+            pRefl->GetRepeatedUInt32(msg, &field, index));
         return "";
     case Fd::CPPTYPE_UINT64:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedUInt64(msg, pField, index));
+            pRefl->GetRepeatedUInt64(msg, &field, index));
         return "";
     case Fd::CPPTYPE_DOUBLE:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedDouble(msg, pField, index));
+            pRefl->GetRepeatedDouble(msg, &field, index));
         return "";
     case Fd::CPPTYPE_FLOAT:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedFloat(msg, pField, index));
+            pRefl->GetRepeatedFloat(msg, &field, index));
         return "";
     case Fd::CPPTYPE_BOOL:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedBool(msg, pField, index));
+            pRefl->GetRepeatedBool(msg, &field, index));
         return "";
     case Fd::CPPTYPE_ENUM:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedEnumValue(msg, pField, index));
+            pRefl->GetRepeatedEnumValue(msg, &field, index));
         return "";
     case Fd::CPPTYPE_STRING:
         SetTblValueElement(rTbl, index,
-            pRefl->GetRepeatedString(msg, pField, index));
+            pRefl->GetRepeatedString(msg, &field, index));
         return "";
     case Fd::CPPTYPE_MESSAGE:
         // Support map entry element.
         SetTblMessageElement(rTbl, index,
-            pRefl->GetRepeatedMessage(msg, pField, index));
+            pRefl->GetRepeatedMessage(msg, &field, index));
         return "";
     default:
         break;
     }
     // Unknown repeated field type CPPTYPE_UNKNOWN of Message.Field
     return string("Unknown repeated field type ") +
-        pField->CppTypeName(eCppType) + " of " +
-        msg.GetTypeName() + "." + pField->name();
+        field.CppTypeName(eCppType) + " of " + field.full_name();
 }
 
 // returns (TableRef, "") or (nil, error_string)
 // Map is supported.
-LuaRef GetRepeatedField(lua_State* L,
-    const Message& msg, const FieldDescriptor* pField)
+LuaRef GetRepeatedField(lua_State& luaState,
+    const Message& msg, const FieldDescriptor& field)
 {
-    assert(L);
-    assert(pField->is_repeated());
+    assert(field.is_repeated());
     const Reflection* pRefl = msg.GetReflection();
     assert(pRefl);
 
-    LuaRef tbl = LuaRef::createTable(L);
-    int nFldSize = pRefl->FieldSize(msg, pField);
+    LuaRef tbl = LuaRef::createTable(&luaState);
+    int nFldSize = pRefl->FieldSize(msg, &field);
     for (int index = 0; index < nFldSize; ++index)
     {
-        string sError = GetRepeatedFieldElement(msg, pField, index, tbl);
+        string sError = GetRepeatedFieldElement(msg, field, index, tbl);
         if (!sError.empty())
             throw LuaException(sError);
     }
@@ -141,7 +138,7 @@ LuaRef GetRepeatedField(lua_State* L,
 
 }  // namespace
 
-LuaRef MessageGetField(lua_State& rLuaState,
+LuaRef GetMessageField(lua_State& rLuaState,
     const Message& msg, const FieldDescriptor& field)
 {
     const Descriptor* pDesc = msg.GetDescriptor();
@@ -157,34 +154,33 @@ LuaRef MessageGetField(lua_State& rLuaState,
     }
 
     using Fd = FieldDescriptor;
-    const Fd* pField = &field;
     lua_State* L = &rLuaState;
     Fd::CppType eCppType = field.cpp_type();
     switch (eCppType)
     {
     // Scalar field always has a default value.
     case Fd::CPPTYPE_INT32:
-        return LuaRefValue(L, pRefl->GetInt32(msg, pField));
+        return LuaRefValue(L, pRefl->GetInt32(msg, &field));
     case Fd::CPPTYPE_INT64:
-        return LuaRefValue(L, pRefl->GetInt64(msg, pField));
+        return LuaRefValue(L, pRefl->GetInt64(msg, &field));
     case Fd::CPPTYPE_UINT32:
-        return LuaRefValue(L, pRefl->GetUInt32(msg, pField));
+        return LuaRefValue(L, pRefl->GetUInt32(msg, &field));
     case Fd::CPPTYPE_UINT64:
-        return LuaRefValue(L, pRefl->GetUInt64(msg, pField));
+        return LuaRefValue(L, pRefl->GetUInt64(msg, &field));
     case Fd::CPPTYPE_DOUBLE:
-        return LuaRefValue(L, pRefl->GetDouble(msg, pField));
+        return LuaRefValue(L, pRefl->GetDouble(msg, &field));
     case Fd::CPPTYPE_FLOAT:
-        return LuaRefValue(L, pRefl->GetFloat(msg, pField));
+        return LuaRefValue(L, pRefl->GetFloat(msg, &field));
     case Fd::CPPTYPE_BOOL:
-        return LuaRefValue(L, pRefl->GetBool(msg, pField));
+        return LuaRefValue(L, pRefl->GetBool(msg, &field));
     case Fd::CPPTYPE_ENUM:
-        return LuaRefValue(L, pRefl->GetEnumValue(msg, pField));
+        return LuaRefValue(L, pRefl->GetEnumValue(msg, &field));
     case Fd::CPPTYPE_STRING:
-        return LuaRefValue(L, pRefl->GetString(msg, pField));
+        return LuaRefValue(L, pRefl->GetString(msg, &field));
     case Fd::CPPTYPE_MESSAGE:
         // For message field, the default value is null.
-        if (pRefl->HasField(msg, pField))
-            return MessageResult(L, pRefl->GetMessage(msg, pField));
+        if (pRefl->HasField(msg, &field))
+            return MessageResult(L, pRefl->GetMessage(msg, &field));
         return LuaRefValue(L, nullptr);
     default:
         break;
