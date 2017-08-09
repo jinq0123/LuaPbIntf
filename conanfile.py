e2a99f01dc46f98c52315fa632401f316a7b0e80
Jin Qing <jinq0123@163.com>
2017-08-09 16:27:51 +0800

    conan new luaphintf/0.1 -t -s

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, CMake
+
+
+class LuaphintfConan(ConanFile):
+    name = "luaphintf"
+    version = "0.1"
+    license = "<Put the package license here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "Description of Luaphintf"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    exports_sources = "src/*"
+
+    def build(self):
+        cmake = CMake(self)
+        self.run('cmake %s/src %s' % (self.source_folder, cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.h", dst="include", src="src")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.dylib*", dst="lib", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["hello"]
