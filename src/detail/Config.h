b3402a87a6fcc109a27bade80efa21418b32a54f
Jin Qing <jinq0123@163.com>
2018-08-03 17:59:00 +0800

    Add extern C in Config.h.

--- src/detail/Config.h
+++ src/detail/Config.h
@@ -1,7 +1,16 @@
 #ifndef DETAIL_CONFIG_H
 #define DETAIL_CONFIG_H
 
+#if !LUAINTF_LINK_LUA_COMPILED_IN_CXX
+extern "C"
+{
+#endif
+
 #include <lua.h>  // for LUA_VERSION_NUM
+
+#if !LUAINTF_LINK_LUA_COMPILED_IN_CXX
+}
+#endif
 
 // Lua5.3 has 64-bit integers.
 #if LUA_VERSION_NUM >= 503
