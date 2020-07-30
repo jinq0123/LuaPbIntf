781761b3557c5a08b47446ad10b30d83d4b79ddf
Jin Qing <jq2019a@163.com>
2020-07-30 11:46:24 +0800

    upgrade to protobuf/3.11.4

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class LuapbintfConan(ConanFile):
     name = "luapbintf"
-    version = "0.1"
+    version = "0.2"
     license = "MIT"
     url = "https://github.com/jinq0123/LuaPbIntf"
     description = "Binding Protobuf 3 to Lua 5.3"
@@ -13,7 +13,7 @@ class LuapbintfConan(ConanFile):
     exports_sources = "src/*"
     requires = ("lua-cpp/5.3.4@jinq0123/testing",
                 "lua-intf/0.1@jinq0123/testing",
-                "protobuf/3.5.1@bincrafters/stable")
+                "protobuf/3.11.4")
 
     def build(self):
         cmake = CMake(self)
